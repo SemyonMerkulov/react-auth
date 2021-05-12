1ac6548f067900bbd889c75b823b04ee99b85355
SemyonMerkulov <semen.merkulov@gmail.com>
2021-05-12 20:38:44 +0700

    add reducers

---
+++ src/store/reducers/userReducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+  user: {},
+  loading: false,
+  error: null
+}
+
+export const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOGIN_USER':
+      return {...state, loading: true}
+    case 'LOGIN_USER_SUCCESS':
+      return {...state, loading: false, user: action.payload}
+    case 'LOGIN_USER_ERROR':
+      return {...state, loading: false, error: action.payload}  
+    case 'LOGOUT_USER':
+      return {...state, user: {}}
+    default:
+      return state
+  }
+}
